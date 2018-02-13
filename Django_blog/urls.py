b5ec15f2f97b298813fd0fe43b2bf8e8dc2ca14a
Akshay Gawali <akshay.jgd21@gmail.com>
2018-02-13 01:07:40 -0800

    Added templates and queryset managers

--- Django_blog/urls.py
+++ Django_blog/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 urlpatterns = [
     # Examples:
     # url(r'^$', 'Django_blog.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
 ]
